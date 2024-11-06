5163e4a120e7e8a14f2a9a6911a8937c31129ff8
RamedicasPredicciones <laura.aguirre@ramedicas.com>
2024-11-06 11:54:22 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Cargar archivos privados de manera segura
 @st.cache_data
 def load_private_files():
-    # Enlace directo para Inventario desde Google Drive (actualizado)
+    # Enlace directo para Inventario desde Google Drive
     inventario_url = "https://drive.google.com/uc?export=download&id=1WV4la88gTl6OUgqQ5UM0IztNBn_k4VrC"
     
     # Cargar ambos archivos, uno desde Drive y el otro localmente
@@ -32,47 +32,48 @@ def procesar_faltantes(faltantes_df, maestro_moleculas_df, inventario_api_df):
         suffixes=('_alternativas', '_inventario')
     )
 
-    alternativas_disponibles_df = alternativas_inventario_df[ 
-        (alternativas_inventario_df['cantidad'] > 0) &
-        (alternativas_inventario_df['codArt_alternativas'].isin(codArt_faltantes))
+    # Cambié 'cantidad' por 'unidadesLote'
+    alternativas_disponibles_df = alternativas_inventario_df[
+        (alternativas_inventario_df['unidadeslote'] > 0) &
+        (alternativas_inventario_df['codart_alternativas'].isin(codArt_faltantes))
     ]
 
-    alternativas_disponibles_df.rename(columns={ 
-        'codArt_alternativas': 'codArt_faltante',
+    alternativas_disponibles_df.rename(columns={
+        'codart_alternativas': 'codart_faltante',
         'opcion_inventario': 'opcion_alternativa',
-        'codArt_inventario': 'codArt_alternativa'
+        'codart_inventario': 'codart_alternativa'
     }, inplace=True)
 
     # Agregar la columna `faltante` al hacer merge
     alternativas_disponibles_df = pd.merge(
-        faltantes_df[['cur', 'codArt', 'faltante']],
+        faltantes_df[['cur', 'codart', 'faltante']],
         alternativas_disponibles_df,
-        left_on=['cur', 'codArt'],
-        right_on=['cur', 'codArt_faltante'],
+        left_on=['cur', 'codart'],
+        right_on=['cur', 'codart_faltante'],
         how='inner'
     )
 
-    # Ordenar por 'codArt_faltante' y 'opcion_alternativa' para priorizar las mejores opciones
-    alternativas_disponibles_df.sort_values(by=['codArt_faltante', 'opcion_alternativa'], inplace=True)
+    # Ordenar por 'codart_faltante' y 'opcion_alternativa' para priorizar las mejores opciones
+    alternativas_disponibles_df.sort_values(by=['codart_faltante', 'opcion_alternativa'], inplace=True)
 
     # Seleccionar la mejor alternativa para cada faltante
     mejores_alternativas = []
-    for codArt_faltante, group in alternativas_disponibles_df.groupby('codArt_faltante'):
+    for codArt_faltante, group in alternativas_disponibles_df.groupby('codart_faltante'):
         faltante_cantidad = group['faltante'].iloc[0]
 
         # Filtrar opciones que tienen cantidad mayor o igual al faltante y obtener la mejor
-        mejor_opcion = group[group['cantidad'] >= faltante_cantidad].head(1)
+        mejor_opcion = group[group['unidadeslote'] >= faltante_cantidad].head(1)
 
         if mejor_opcion.empty:
             # Si no hay opción suficiente, tomar la mayor cantidad disponible
-            mejor_opcion = group.nlargest(1, 'cantidad')
+            mejor_opcion = group.nlargest(1, 'unidadeslote')
 
         mejores_alternativas.append(mejor_opcion.iloc[0])
 
     resultado_final_df = pd.DataFrame(mejores_alternativas)
 
     # Seleccionar las columnas finales deseadas
-    columnas_finales = ['cur', 'codArt', 'faltante', 'codArt_faltante', 'opcion_alternativa', 'codArt_alternativa', 'cantidad', 'bodega']
+    columnas_finales = ['cur', 'codart', 'faltante', 'codart_faltante', 'opcion_alternativa', 'codart_alternativa', 'unidadeslote', 'bodega']
     resultado_final_df = resultado_final_df[columnas_finales]
 
     return resultado_final_df
@@ -98,3 +99,4 @@ if uploaded_file:
         file_name='alternativas_disponibles.xlsx',
         mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
     )
+
