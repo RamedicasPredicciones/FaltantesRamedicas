dc07f9df72c70ec8cc7671842176703538fc5f23
RamedicasPredicciones <laura.aguirre@ramedicas.com>
2024-11-21 14:52:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,14 @@ import pandas as pd
 import math
 from io import BytesIO
 
+# Enlace de la plantilla en Google Sheets
+PLANTILLA_URL = "https://docs.google.com/spreadsheets/d/1CPMBfCiuXq2_l8KY68HgexD-kyNVJ2Ml/export?format=xlsx"
+
 # Función para cargar archivo de inventario desde Google Drive
 def load_inventory_file():
     inventario_url = "https://docs.google.com/spreadsheets/d/1WV4la88gTl6OUgqQ5UM0IztNBn_k4VrC/export?format=xlsx&sheet=Hoja3"
     inventario_api_df = pd.read_excel(inventario_url, sheet_name="Hoja3")
     return inventario_api_df
-
-# Función para descargar la plantilla de faltantes
-def descargar_plantilla():
-    with open("Faltantes.xlsx", "rb") as f:
-        plantilla = f.read()
-    return plantilla
 
 # Función para procesar el archivo de faltantes
 def procesar_faltantes(faltantes_df, inventario_api_df, columnas_adicionales, bodega_seleccionada):
@@ -28,7 +25,6 @@ def procesar_faltantes(faltantes_df, inventario_api_df, columnas_adicionales, bo
 
     cur_faltantes = faltantes_df['cur'].unique()
     alternativas_inventario_df = inventario_api_df[inventario_api_df['cur'].isin(cur_faltantes)]
-
 
     if bodega_seleccionada:
         alternativas_inventario_df = alternativas_inventario_df[alternativas_inventario_df['bodega'].isin(bodega_seleccionada)]
@@ -90,11 +86,13 @@ st.markdown(
         Generador de Alternativas para Faltantes
     </h3>
     <p style="text-align: center; font-family: Arial, sans-serif; color: #6B6B6B;">
-        Esta herramienta te permite buscar el código aternativa para cada faltante de los pedidos en Ramédicas con sus respectivo inventario actual.
+        Esta herramienta te permite buscar el código alternativa para cada faltante de los pedidos en Ramédicas con su respectivo inventario actual.
     </p>
     """, unsafe_allow_html=True
 )
 
+# Botón para descargar la plantilla
+st.markdown(f"[Descargar plantilla de faltantes](<{PLANTILLA_URL}>)", unsafe_allow_html=True)
 
 # Botón para actualizar inventario
 if st.button('Actualizar inventario'):
@@ -132,5 +130,5 @@ if uploaded_file:
             label="Descargar archivo de alternativas",
             data=to_excel(resultado_final_df),
             file_name='alternativas_disponibles.xlsx',
-            mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+            mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
