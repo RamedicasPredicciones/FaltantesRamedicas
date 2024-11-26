6c4c4eea242b2b339fd0f521c0602a8d7de7df71
RamedicasPredicciones <laura.aguirre@ramedicas.com>
2024-11-26 08:42:57 -0500

    Update app.py

--- app.py
+++ app.py
@@ -34,7 +34,8 @@ def procesar_faltantes(faltantes_df, inventario_api_df, columnas_adicionales, bo
     alternativas_disponibles_df.rename(columns={
         'codart': 'codart_alternativa',
         'opcion': 'opcion_alternativa',
-        'embalaje': 'embalaje_alternativa'
+        'embalaje': 'embalaje_alternativa',
+        'unidadespresentacionlote': 'Existencias codart alternativa'
     }, inplace=True)
 
     alternativas_disponibles_df = pd.merge(
@@ -55,15 +56,15 @@ def procesar_faltantes(faltantes_df, inventario_api_df, columnas_adicionales, bo
         axis=1
     )
 
-    alternativas_disponibles_df.sort_values(by=['codart', 'unidadespresentacionlote'], inplace=True)
+    alternativas_disponibles_df.sort_values(by=['codart', 'Existencias codart alternativa'], inplace=True)
 
     mejores_alternativas = []
     for codart_faltante, group in alternativas_disponibles_df.groupby('codart'):
         faltante_cantidad = group['faltante'].iloc[0]
 
         # Buscar en la bodega seleccionada
-        mejor_opcion_bodega = group[group['unidadespresentacionlote'] >= faltante_cantidad]
-        mejor_opcion = mejor_opcion_bodega.head(1) if not mejor_opcion_bodega.empty else group.nlargest(1, 'unidadespresentacionlote')
+        mejor_opcion_bodega = group[group['Existencias codart alternativa'] >= faltante_cantidad]
+        mejor_opcion = mejor_opcion_bodega.head(1) if not mejor_opcion_bodega.empty else group.nlargest(1, 'Existencias codart alternativa')
         
         mejores_alternativas.append(mejor_opcion.iloc[0])
 
@@ -71,17 +72,17 @@ def procesar_faltantes(faltantes_df, inventario_api_df, columnas_adicionales, bo
 
     # Nuevas columnas para verificar si el faltante fue suplido y el faltante restante
     resultado_final_df['suplido'] = resultado_final_df.apply(
-        lambda row: 'SI' if row['unidadespresentacionlote'] >= row['cantidad_necesaria'] else 'NO',
+        lambda row: 'SI' if row['Existencias codart alternativa'] >= row['cantidad_necesaria'] else 'NO',
         axis=1
     )
 
     resultado_final_df['faltante_restante'] = resultado_final_df.apply(
-        lambda row: row['cantidad_necesaria'] - row['unidadespresentacionlote'] if row['suplido'] == 'NO' else 0,
+        lambda row: row['cantidad_necesaria'] - row['Existencias codart alternativa'] if row['suplido'] == 'NO' else 0,
         axis=1
     )
 
     columnas_finales = ['cur', 'codart', 'faltante', 'embalaje', 'codart_alternativa', 'opcion_alternativa', 
-                        'embalaje_alternativa', 'cantidad_necesaria', 'unidadespresentacionlote', 'bodega', 'carta', 'suplido', 'faltante_restante']
+                        'embalaje_alternativa', 'cantidad_necesaria', 'Existencias codart alternativa', 'bodega', 'carta', 'suplido', 'faltante_restante']
     columnas_finales.extend([col.lower() for col in columnas_adicionales])
     columnas_presentes = [col for col in columnas_finales if col in resultado_final_df.columns]
     resultado_final_df = resultado_final_df[columnas_presentes]
